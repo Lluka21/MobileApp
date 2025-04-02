aa8884b2973aceba07c91ee25f24721d1adc2274
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-04-02 09:34:55 +0400

    Update FavoritesScreen.js

--- FavoritesScreen.js
+++ FavoritesScreen.js
@@ -5,7 +5,7 @@ import localimages from "../localimages";
 const FavoritesScreen = () => {
   const { favoriteItems } = useFavoritesContext();
 
-// render favorite items
+//  favorite items
   
   const renderFavoriteItem = ({ item }) => {
     const specialSource = localimages[item.image] || { uri: item.image };
