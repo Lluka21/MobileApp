1605f4bc8e6149587c467aa8e3b6b5c0890c430a
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-04-07 00:15:40 +0400

    Update CartScreen.js

--- CartScreen.js
+++ CartScreen.js
@@ -68,7 +68,7 @@ const CartScreen = () => {
           Cart
         </Text>
 
-            // L
+            // 
           <FlatList
           data={cartItems}
           renderItem={renderCartItem}
