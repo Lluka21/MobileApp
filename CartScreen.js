e5cc1935f2048bb5d4997ae3c65cffa65429c8c2
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-04-11 08:52:27 +0400

    Update CartScreen.js

--- CartScreen.js
+++ CartScreen.js
@@ -12,6 +12,8 @@ import localimages from "../localimages";
 import Icon from "react-native-vector-icons/Ionicons";
 
 
+// CartScreen logic
+
 
 const CartScreen = () => {
   const { cartItems, removeFromCart } = useCartContext();
