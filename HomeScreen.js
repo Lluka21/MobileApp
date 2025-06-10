f7169aff49bc2b20f036fa35ec02dbd5f494597d
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-06-10 19:45:26 +0400

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -163,7 +163,7 @@ const HomeScreen = () => {
         keyExtractor={(item) => item}
         horizontal
       />
-      {/* FlatList for Co*/}
+      {/* FlatList for C*/}
       <FlatList
         
         data={categoryItems}
