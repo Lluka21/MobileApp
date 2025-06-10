dfa6ffb26035a9fb55dc86d09173607df7eddaaa
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-06-10 19:42:50 +0400

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -195,7 +195,7 @@ const HomeScreen = () => {
         }
         showsHorizontalScrollIndicator={false}
       />
-      {/* Flatlist for special offers end here */}
+      {/* Flatlist for special offers end her*/}
     </ScrollView>
   );
 };
