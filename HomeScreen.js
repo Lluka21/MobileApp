d4f8f253fa373b01228a7d2099825ce0b75f9fb9
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-06-22 11:50:02 +0400

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -135,7 +135,7 @@ const HomeScreen = () => {
       </View>
       <Text style={styles.categoryTitle}>Categories</Text>
 
-      {/* Fl */}
+      {/* F */}
       <FlatList
         data={Object.keys(coffeeData.Categories)}
         showsHorizontalScrollIndicator={false}
