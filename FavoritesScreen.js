a201d4c1ff87276194515a3f74ba523fadb263cd
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-07-08 09:50:47 +0400

    Update FavoritesScreen.js

--- FavoritesScreen.js
+++ FavoritesScreen.js
@@ -5,7 +5,7 @@ import localimages from "../localimages";
 
 const FavoritesScreen = () => {
 
-    // Destructuring co
+    // Destructuring con
   const { favoriteItems } = useFavoritesContext();
   
   
