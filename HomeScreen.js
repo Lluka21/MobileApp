b843f519ed7c777940c845b134a994cc8f1cab9f
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2026-02-06 20:14:18 +0400

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -18,7 +18,7 @@ import specialData from "../data/special.json";
 import { useFavoritesContext } from "../context/FavoritesContext";
 import { useCartContext } from "../
 
-  /
+  
 
 const HomeScreen = () => {
   const [selectedCategory, setSelectedCategory] = useState("Cappuccino");
@@ -304,3 +304,4 @@ const styles = StyleSheet.create({
     height: 240,
   },
 });
+
